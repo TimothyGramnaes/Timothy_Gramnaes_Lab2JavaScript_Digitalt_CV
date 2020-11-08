32a2d342be25ad1dcbb135511fd831f2695113fd
Timothy Gramnaes <timothy.gramnaes@medieinstitutet.se>
2020-11-08 22:00:54 +0100

    Added Dice in Header

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-
+/**
+ * Dice roll on header
+ */
 
 function rollDice() {
     var die = document.getElementById("die");
 
     var d1 = Math.floor(Math.random() * 6) +1;
-    die.innerHTML = d1;
+        if (d1 == 1) {
+            die.innerHTML = '<img src="imgs/dice/1.png" alt="" srcset="">';
+        }
+        if (d1 == 2) {
+            die.innerHTML = '<img src="imgs/dice/2.png" alt="" srcset="">';
+        }
+        if (d1 == 3) {
+            die.innerHTML = '<img src="imgs/dice/3.png" alt="" srcset="">';
+        }
+        if (d1 == 4) {
+            die.innerHTML = '<img src="imgs/dice/4.png" alt="" srcset="">';
+        }
+        if (d1 == 5) {
+            die.innerHTML = '<img src="imgs/dice/5.png" alt="" srcset="">';
+        }
+        if (d1 == 6) {
+            die.innerHTML = '<img src="imgs/dice/1.png" alt="" srcset="">';
+        }
 
-    console.log(d1);
 }
