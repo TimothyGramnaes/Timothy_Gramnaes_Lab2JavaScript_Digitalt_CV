3637f1c9bfdabe942757c25d64ceae75496f4a81
Timothy Gramnaes <timothy.gramnaes@medieinstitutet.se>
2020-11-11 22:47:54 +0100

    Links and contacts

--- script.js
+++ script.js
@@ -14,22 +14,22 @@ function rollDice() {
     const die = document.getElementById("die");
     let d1 = Math.floor(Math.random() * 6) +1;
         if (d1 == 1) {
-            die.innerHTML = '<img src="imgs/dice/1.png" alt="" srcset="">';
+            die.innerHTML = '<img src="../imgs/dice/1.png" alt="" srcset="">';
         }
         if (d1 == 2) {
-            die.innerHTML = '<img src="imgs/dice/2.png" alt="" srcset="">';
+            die.innerHTML = '<img src="../imgs/dice/2.png" alt="" srcset="">';
         }
         if (d1 == 3) {
-            die.innerHTML = '<img src="imgs/dice/3.png" alt="" srcset="">';
+            die.innerHTML = '<img src="../imgs/dice/3.png" alt="" srcset="">';
         }
         if (d1 == 4) {
-            die.innerHTML = '<img src="imgs/dice/4.png" alt="" srcset="">';
+            die.innerHTML = '<img src="../imgs/dice/4.png" alt="" srcset="">';
         }
         if (d1 == 5) {
-            die.innerHTML = '<img src="imgs/dice/5.png" alt="" srcset="">';
+            die.innerHTML = '<img src="../imgs/dice/5.png" alt="" srcset="">';
         }
         if (d1 == 6) {
-            die.innerHTML = '<img src="imgs/dice/6.png" alt="" srcset="">';
+            die.innerHTML = '<img src="../imgs/dice/6.png" alt="" srcset="">';
         }
 
 }
