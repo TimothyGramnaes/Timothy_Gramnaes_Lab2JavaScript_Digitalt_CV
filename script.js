91e7ac25e1275f41ac177ed41d665f8c9a045c6e
Timothy Gramnaes <timothy.gramnaes@medieinstitutet.se>
2020-11-12 11:02:25 +0100

    fixingsss

--- script.js
+++ script.js
@@ -11,16 +11,21 @@ function startPage() {
  * Dice roll on header
  */
 function rollDice() {
+   // const image = document.createElement('img');
+    
     const die = document.getElementById("die");
     let d1 = Math.floor(Math.random() * 6) +1;
-        if (d1 == 1) {
-            die.innerHTML = '<img src="./imgs/dice/1.png" alt="" srcset="">';
+    if (d1 == 1) {
+        die.innerHTML = '<img src="./imgs/dice/1.png" alt="" srcset="">';
+        //image.src = './imgs/dice/1.png';
         }
         if (d1 == 2) {
             die.innerHTML = '<img src="./imgs/dice/2.png" alt="" srcset="">';
+          // image.src = './imgs/dice/2.png';
         }
         if (d1 == 3) {
             die.innerHTML = '<img src="./imgs/dice/3.png" alt="" srcset="">';
+           // image.src = './imgs/dice/3.png';
         }
         if (d1 == 4) {
             die.innerHTML = '<img src="./imgs/dice/4.png" alt="" srcset="">';
@@ -31,6 +36,8 @@ function rollDice() {
         if (d1 == 6) {
             die.innerHTML = '<img src="./imgs/dice/6.png" alt="" srcset="">';
         }
+       // die.innerHTML = ""
+      //  die.append(image)
 
 }
 
